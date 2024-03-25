ec2501fc67445a26eb7e7e4ff33f3882adea063b
HaddoucheMilissa <melissahaddo18@gmail.com>
2024-03-26 00:48:51 +0100

    add SVM as a class

--- SVM.py
+++ SVM.py
@@ -11,7 +11,6 @@ x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.2)
 classifier1=SVC(kernel='linear',C=5 )
 classifier1.fit(x_train,y_train)
 
-
 #result is : 0.956140350877193
 #create 2nd classifier
 
@@ -19,3 +18,27 @@ classifier2=KNeighborsClassifier(n_neighbors=4)
 classifier2.fit(x_train,y_train)
 print("Score of the 1st classifier(SVC) :",classifier1.score(x_test,y_test)) # Score of the 1st classifier(SVC) : 0.9210526315789473
 print("Score of the 2nd classifier(SVC) :",classifier2.score(x_test,y_test)) # Score of the 2nd classifier(SVC) : 0.9385964912280702
+
+#SVM as a class
+
+import numpy as np
+class SVM:
+    def __init__(self,learning_rate=0.001,lambda_para=0.01,nb_iterations=1000):
+        self.lr=learning_rate
+        self.lambda_para= lambda_para
+        self.nb_iterations=nb_iterations
+        self.w=None
+        self.b=None
+        
+    def fit(self,x,y):
+        yy=np.where(y<=0,-1,1)
+        nb_samples,nb_features=x.shape
+        # nb rows = nb samples
+        # nb columns = nb features
+        self.w=np.zeros(nb_features)
+        self.b=0
+        for i in range(self.nb_iterations):
+              for index, x_index in enumerate(x):
+                  cond= y [index]* (np.dot(x_index,self.w)-self.b)>=1
+                  if cond:
+                        self.w -= self.lr (2*self.lambda_para *self.w )
