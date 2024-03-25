654fcefad090d2de87e4926e47832b84738a936b
HaddoucheMilissa <melissahaddo18@gmail.com>
2024-03-26 00:41:33 +0100

    create 2nd classifier

--- SVM.py
+++ SVM.py
@@ -13,3 +13,9 @@ classifier1.fit(x_train,y_train)
 
 
 #result is : 0.956140350877193
+#create 2nd classifier
+
+classifier2=KNeighborsClassifier(n_neighbors=4)
+classifier2.fit(x_train,y_train)
+print("Score of the 1st classifier(SVC) :",classifier1.score(x_test,y_test)) # Score of the 1st classifier(SVC) : 0.9210526315789473
+print("Score of the 2nd classifier(SVC) :",classifier2.score(x_test,y_test)) # Score of the 2nd classifier(SVC) : 0.9385964912280702
