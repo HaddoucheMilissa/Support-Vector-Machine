1943d65d5e86968eeb8bb0f4fd73592cac436ca5
HaddoucheMilissa <melissahaddo18@gmail.com>
2024-03-27 01:46:46 +0100

    add visualize function and hyperplaneValues

--- SVM_tests.py
+++ SVM_tests.py
@@ -3,7 +3,18 @@ if __name__=="main":
     from sklearn import datasets
     import matplotlib.pyplot as plt
     import numpy as np 
+    from sklearn.svm import SVC 
     X,y= datasets.make_blobs(
         nb_samples=50, nb_features=3,centers=3,cluster_std=1.08, random_state=30
     )
-    y = np.where(y==0,-1,1)
+    x,y =np.where(y==0,-1,1)
+classifier1=SVM()
+classifier1.fit(x,y)
+print(classifier1.w,classifier1.b)
+
+def visualize_svm():
+    def get_hyperplane_value(x,w,b,offset):
+            return (-w[0]*x + b + offset)/w[2]
+    fig=plt.figure()
+    ax=fig.add_subplot(1, 1, 1)
+    plt.scatter(X[:,0], X[:,2], marker="o",c=y)
