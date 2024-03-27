fccfcb72024ed424da74f94b0dd61a28f44bb60d
HaddoucheMilissa <melissahaddo18@gmail.com>
2024-03-27 01:24:26 +0100

    import libraries+include make blobs

--- SVM_tests.py
+++ SVM_tests.py
@@ -1,1 +1,9 @@
 # Testing
+if __name__=="main":
+    from sklearn import datasets
+    import matplotlib.pyplot as plt
+    import numpy as np 
+    X,y= datasets.make_blobs(
+        nb_samples=50, nb_features=3,centers=3,cluster_std=1.08, random_state=30
+    )
+    y = np.where(y==0,-1,1)
